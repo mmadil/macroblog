449c8dd4b0ba287741912d0f4ea3155de652227d
Mohammad Adil <mmadil_14@yahoo.com>
2013-07-09 02:41:14 +0530

    settings changed for elasticsearch

--- macroblog/settings.py
+++ macroblog/settings.py
@@ -32,7 +32,7 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 HAYSTACK_CONNECTIONS = {
         'default': {
                     'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-                    'URL': 'http://127.0.0.1:9200/',
+                    'URL': 'http://0.0.0.0:9200/',
                     'INDEX_NAME': 'haystack',
             },
 }
