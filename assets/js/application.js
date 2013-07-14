1d0a4a72d573e5cb5a556b29e48d0aafb72fde1d
Mohammad Adil <mmadil_14@yahoo.com>
2013-07-14 23:15:14 +0530

    making another branch for jquery

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,8 +1,3 @@
 jQuery(document).ready(function(){
 
-  var help-text = $('<p>To understand more about markdown please have a look at the following <a href="http://stackoverflow.com/editing-help">link</a>.</p>');
-
-  $("input[type=text]").focus(function () {
-    $(this).next("p").css('display','inline').fadeOut(1000);
-});
 });
