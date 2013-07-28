01c8cb4b3eb6e1b180f6c92cc6e49fd97051c8e6
Mohammad Adil <mmadil_14@yahoo.com>
2013-07-29 00:59:34 +0530

    I have a new theme now

--- widgets/templatetags/macro.py
+++ widgets/templatetags/macro.py
@@ -3,8 +3,9 @@ from urlparse import urlparse
 
 register = template.Library()
 
-
-def website(url):
-    u = urlparse(url)
-    return u[1]
-
+@register.simple_tag
+def active(path, pattern):
+    import re
+    if re.search(pattern, path):
+        return 'active'
+    return ''
